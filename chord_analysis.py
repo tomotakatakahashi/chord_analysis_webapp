dd594c1176b88132aec68f445f9a57d4138f2b17
Tomotaka Takahashi <domefasoltedo@gmail.com>
2019-08-31 18:42:09 -0700

    Update a module for performance

--- chord_analysis.py
+++ chord_analysis.py
@@ -47,13 +47,11 @@ CHORD_NAMES, CHORD_FEATURES = _generate_chords(
 
 def audio_wave_to_chromagram_seq(audio_wave, sampling_rate):
     """Audio wave to Chromagram Seq"""
-    y_harmonic, y_percussive = librosa.effects.hpss(audio_wave)
-
     chromagram = librosa.feature.chroma_cqt(
-        y=y_harmonic, sr=sampling_rate, bins_per_octave=36
+        y=audio_wave, sr=sampling_rate, bins_per_octave=36
     )
 
-    _, beats = librosa.beat.beat_track(y=y_percussive, sr=sampling_rate)
+    _, beats = librosa.beat.beat_track(y=audio_wave, sr=sampling_rate)
     timeline = librosa.frames_to_time(np.concatenate(([0], beats)))
 
     chromagram_sync = librosa.util.sync(chromagram, beats, aggregate=np.median).T
@@ -68,7 +66,7 @@ def chromagram_seq_to_chord_seq(chromagram_seq):
 
 def audio_path_to_chord_seq(audio_path):
     """Audio path to Chord seq"""
-    audio_wave, sampling_rate = librosa.load(audio_path)
+    audio_wave, sampling_rate = librosa.load(audio_path, res_type="kaiser_fast")
     chromagram_seq, timeline = audio_wave_to_chromagram_seq(audio_wave, sampling_rate)
     chord_seq = chromagram_seq_to_chord_seq(chromagram_seq)
     return chord_seq, timeline
