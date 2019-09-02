bb4bf9bb482c8745ab68690ecb668e97206c9866
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2019-09-02 06:53:31 -0700

    Make this a web application

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Web Application to Analyze Chord Progression of Songs
 
-WIP.
+## Usage
+```
+git clone <this repository>
+pip3 install gunicorn flask librosa
+gunicorn app:app
+```
+
+See http://127.0.0.1:8000/ and upload a MP3 file.
---
+++ app.py
@@ -0,0 +1,55 @@
+"""Web App by Flask"""
+import os
+import tempfile
+
+from flask import Flask, request, redirect, url_for, render_template, jsonify
+
+import chord_analysis
+
+ALLOWED_EXTENSIONS = {"mp3", }
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(12)
+
+
+def allowed_file(filename):
+    """File Extension Checker"""
+    return '.' in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=["GET"])
+def index_page():
+    """Render the Index Page"""
+    return render_template("index.html")
+
+@app.route("/license", methods=["GET"])
+def license_page():
+    """Render License page"""
+    return render_template("license.html")
+
+@app.route('/chord_tracker', methods=['GET', 'POST'])
+def upload_file():
+    """API part"""
+    if request.method == 'POST':
+        if "fileIpt" not in request.files or request.files["fileIpt"].filename == "":
+            return jsonify(
+                {
+                    "result": "failure",
+                    "message": "No file part",
+                }
+            )
+        file = request.files["fileIpt"]
+        if file and allowed_file(file.filename):
+            with tempfile.NamedTemporaryFile(mode="wb", delete=False) as tmp_file:
+                tmp_file.write(file.read())
+            chord_seq, timeline = chord_analysis.audio_path_to_chord_seq(
+                tmp_file.name
+            )
+            return jsonify(
+                {
+                    "result": "success",
+                    "chord_seq": chord_seq,
+                    "timeline": timeline.tolist(),
+                }
+            )
+    return redirect(url_for("index_page"))
