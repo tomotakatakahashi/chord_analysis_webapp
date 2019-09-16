f9c728b9b6037287d567cea4c5f2c68ff3e8536f
Tomotaka Takahashi <domefasoltedo@gmail.com>
2019-09-15 18:06:35 -0700

    Add a sample audio

--- static/index.js
+++ static/index.js
@@ -1,41 +1,79 @@
 var chordSeq = [""];
 var timeline = [0];
-const fileIpt = document.getElementById("fileIpt");
-const audioPlayer = document.getElementById("audioPlayer");
-function handleFiles(files){
+const audioFileObjectURLs = []; // URLs to revoke
+
+
+function sampleOnClick(){
+    const sampleAudioURL = "static/sample-pachelbel.mp3";
+    const sampleChordSeq = ["GM7", "G", "G", "G", "G", "D", "D", "D", "G", "G", "G", "G", "A7", "A", "A", "A", "D", "D", "D", "D", "DM7", "A", "A", "A", "B", "B", "B", "Bm", "Bm", "D", "D", "F#m", "G", "G", "G", "G", "G", "D", "D", "D", "G", "G", "G", "G", "A7", "A", "A", "A", "D", "D", "D", "D", "DM7", "A", "A", "A", "Bm7", "B", "Bm", "Bm", "Bm", "D", "D", "F#m", "GM7", "G", "G", "G", "D", "D", "D", "D", "G", "G", "G", "G", "A7", "A", "A", "A", "D", "D", "D", "D", "A"];
+    const sampleTimeLine = [0, 0.3947392290249433, 0.8126984126984127, 1.230657596371882, 1.6718367346938776, 2.1130158730158732, 2.5541950113378684, 2.972154195011338, 3.3901133786848074, 3.8080725623582765, 4.249251700680272, 4.713650793650793, 5.178049886621316, 5.61922902494331, 6.013968253968254, 6.408707482993197, 6.8034467120181406, 7.198185941043084, 7.616145124716553, 8.010884353741497, 8.452063492063493, 8.846802721088435, 9.241541950113378, 9.659501133786849, 10.12390022675737, 10.541859410430838, 10.95981859410431, 11.377777777777778, 11.795736961451247, 12.19047619047619, 12.60843537414966, 12.979954648526077, 13.397913832199546, 13.79265306122449, 14.164172335600908, 14.489251700680272, 14.86077097505669, 15.255510204081633, 15.650249433106575, 16.091428571428573, 16.555827664399093, 16.950566893424035, 17.345306122448978, 17.786485260770974, 18.204444444444444, 18.622403628117915, 19.086802721088436, 19.52798185941043, 20.038820861678005, 20.503219954648525, 20.897959183673468, 21.31591836734694, 21.71065759637188, 22.128616780045352, 22.523356009070294, 22.941315192743765, 23.336054421768708, 23.754013605442175, 24.14875283446712, 24.589931972789117, 25.007891156462584, 25.425850340136055, 25.843809523809522, 26.261768707482993, 26.679727891156464, 27.074467120181406, 27.515646258503402, 27.910385487528345, 28.328344671201815, 28.746303854875283, 29.141043083900225, 29.53578231292517, 29.95374149659864, 30.34848072562358, 30.743219954648527, 31.161179138321994, 31.579138321995465, 31.997097505668933, 32.43827664399093, 32.879455782312924, 33.274195011337866, 33.69215419501134, 34.11011337868481, 34.5512925170068, 34.969251700680275];
+
+    refreshAudioPlayerBlock(sampleAudioURL);
+    refreshChordView(sampleChordSeq, sampleTimeLine);
+}
+
+
+
+function refreshAudioPlayerBlock(audioFileURL){
+    const audioPlayerBlock = document.getElementById("audioPlayerBlock");
+
+    audioPlayerBlock.innerHTML = "";
+    audioFileObjectURLs.forEach(function(elem){
+        URL.revokeObjectURL(elem);
+    });
+    audioFileObjectURLs.length = 0;
+
+    const audio = document.createElement("audio");
+    audio.controls = true;
+    audio.id = "audio";
+    audio.src = audioFileURL;
+    audioPlayerBlock.appendChild(audio);
+
+    const errorMessage = document.createElement("p");
+    errorMessage.innerHTML = "Your browser cannot play music. Please use a modern browser.";
+    audio.appendChild(errorMessage);
+}
+
+function refreshChordView(cSeq, tLine){
+    chordView.innerHTML = "<p>Ready.</p>";
+    chordSeq = cSeq;
+    timeline = tLine;
+
+    const audio = document.getElementById("audio");
+    audio.addEventListener("timeupdate", function(event){
+        var chordIdx = 0;
+        // timeline[chordIdx] <= this.currentTime < timeline[chordIdx + 1]
+        while(chordIdx + 1 < chordSeq.length && timeline[chordIdx + 1] <= this.currentTime){
+            chordIdx++;
+        }
+        const chordView = document.getElementById("chordView");
+        chordView.innerHTML = "<p>" + chordSeq[chordIdx] + "</p>";
+    });
+}
+
+function fileIptOnChange(files){
     if(!files.length){
         audioPlayer.innerHTML = "<p>No audio selected.</p>";
     }else{
-        const audio = document.createElement("audio");
-        audio.controls = true;
-        audio.src = window.URL.createObjectURL(files[0]);
-        audioPlayer.appendChild(audio);
+        const audioFileObjectURL = URL.createObjectURL(files[0]);
+        refreshAudioPlayerBlock(audioFileObjectURL);
+        audioFileObjectURLs.push(audioFileObjectURL);
 
-        const errorMessage = document.createElement("p");
-        errorMessage.innerHTML = "Your browser cannot play musics. Please use a modern browser.";
-        audio.appendChild(errorMessage);
-        const chordView = document.getElementById("chordView");
+        const formData = new FormData();
+        formData.set("fileIpt", files[0]);
 
+        const audio = document.getElementById("audio");
         chordView.innerHTML = "<p>Loading. Please wait for 30 seconds.</p>";
         $.ajax({
             url: "chord_tracker",
             type: "POST",
             dataType: "json",
             contentType: false,
-            data: new FormData($("#fileForm").get(0)),
+            data: formData,
             processData: false
         }).done(
             function(res){
-                chordView.innerHTML = "<p>Ready.</p>";
-                chordSeq = res.chord_seq;
-                timeline = res.timeline;
-                audio.addEventListener("timeupdate", (event) => {
-                    var chordIdx = 0;
-                    while(chordIdx < chordSeq.length && timeline[chordIdx] < audio.currentTime){
-                        chordIdx++;
-                    }
-                    chordView.innerHTML = "<p>" + chordSeq[chordIdx] + "</p>";
-                });
+                refreshChordView(res.chord_seq, res.timeline);
             }
         ).fail(
             function(res){
